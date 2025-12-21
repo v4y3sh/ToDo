72b2643a47f0bbe0cc0722946b8695bf79af5fc0
v4y3sh <vishwavaishnavi.one@gmail.com>
2025-12-21 22:52:05 +0530

    added saveTask func on local storage

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ const taskBtn = document.getElementById('add-task-btn');
 const todoList = document.getElementById('todo-list');
 
 //add task array
-let tasks = [];
+let tasks = JSON.parse(localStorage.getItem('tasks')) || [];    //if nothing to parse, then acts like empty array
+
+tasks.forEach( (task) => renderTask(task));
 
 //trim removes extra spaces
 taskBtn.addEventListener('click', () => {
@@ -12,13 +14,24 @@ taskBtn.addEventListener('click', () => {
     if ( taskTest === "") return
 
     const newTask = {       //creating new object w 3 properties
-        id: Date.now(),
+        id: Date.now(),     // bad idea to use array-index ids as after adding or deleting tasks, index changes
         text: taskTest,
         completed: false,
     };
     tasks.push(newTask);
+    saveTasks();            //can be seen under Application heading
     todoInput.value ="" //clear input
     console.log(tasks);
 
 });
 
+function saveTasks (){
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+
+//read from local storage nd render into DOM
+
+function renderTask(task){
+    console.log(task);
+}
